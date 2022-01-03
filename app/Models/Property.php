d56eedb1b047f530e7f5ebf7d74e997c7eb4e9c4
zahidhasanbd121 <zahidhasan.limon121@gmail.com>
2022-01-03 13:32:24 +0600

    Part #7

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Property extends Model
 {
     use HasFactory;
+
+    public function featured() {
+        $this->belongsTo(Media::class, 'featured_media_id');
+    }
+    public function location() {
+        $this->belongsTo(Location::class, 'location_id');
+    }
+    public function gallery() {
+        $this->hasMany(Media::class, 'property_id');
+    }
 }
