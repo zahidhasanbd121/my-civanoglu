315e73fbf00aea63c614a2934e5c3d3d95751cd9
zahidhasanbd121 <zahidhasan.limon121@gmail.com>
2022-01-07 22:57:35 +0600

    Part #15 Single Property Form Data Transfer

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function propertyInquiry(Request $request, $property_id) {
+        $request->validate([
+            'name' => 'required|max:255',
+            'phone' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'message' => 'required|max:255',
+        ]);
+
+        $contact = new Contact();
+        $contact->name = $request->name;
+        $contact->phone = $request->phone;
+        $contact->email = $request->email;
+        $contact->message = $request->message . '\n This message has been sent via ' . route('single-property', $property_id) . 'website.';
+        $contact->save();
+
+        // send user & admin message
+
+        return redirect(route('single-property', $property_id));
+    }
+
+
+}
