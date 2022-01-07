7646fb868e83695c1bc2f53227ed5b445175faf7
zahidhasanbd121 <zahidhasan.limon121@gmail.com>
2022-01-07 11:42:43 +0600

    Part #14 Location dynamic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     <div class="-mt-10">
         <div class="container">
             <div class="rounded-lg bg-white p-4 relative z-20 shadow-lg home-search">
-                @include('components.property-search-form')
+                @include('components.property-search-form', ['locations' => $locations])
             </div>
 
         </div>
