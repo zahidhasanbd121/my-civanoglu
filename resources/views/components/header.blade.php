04e56fb1e89bfc19b88f79c86382caf13c318f3f
zahidhasanbd121 <zahidhasan.limon121@gmail.com>
2022-01-08 18:03:53 +0600

    Part #17 Mcamara/LaravelLocalization install

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,7 +6,7 @@
     <div class="w-full">
         <ul class="flex justify-center">
             <li><a class="inline-block p-4 text-white" href="{{route('properties')}}?type=0">Land</a></li>
-            <li><a class="inline-block p-4 text-white" href="{{route('properties')}}?type=2">Villa</a></li>
+            <li><a class="inline-block p-4 text-white" href="{{route('properties')}}?type=2">{{__('Villa')}}</a></li>
             <li><a class="inline-block p-4 text-white" href="{{route('properties')}}?type=1">Apartment</a></li>
             <li><a class="inline-block p-4 text-white" href="{{route('page', 'about-us')}}">About Us</a></li>
             <li><a class="inline-block p-4 text-white" href="{{route('page', 'contact-us')}}">Contact Us</a></li>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PropertyController;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,12 +22,17 @@ use App\Http\Controllers\PropertyController;
 //     return view('welcome');
 // });
 
+Route::group(['prefix' => LaravelLocalization::setLocale()], function() {
+    
 Route::get('/', [HomeController::class, 'home'])->name('home');
 //Route::get('/email-test', [ContactController::class, 'test']);
 Route::get('/property/{id}', [PropertyController::class, 'single'])->name('single-property');
 Route::get('/properties/', [PropertyController::class, 'index'])->name('properties');
 Route::get('/page/{slug}', [PageController::class, 'single'])->name('page');
 Route::post('/property-inquiry/{id}',  [ContactController::class, 'propertyInquiry'])->name('property-inquiry');
+});
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
